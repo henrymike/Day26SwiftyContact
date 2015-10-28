f60012e601d7c9f52a11b7f20307ab980758a9a4
Mike Henry <chsmike@gmail.com>
2015-10-28 16:42:31 -0400

    Logging obtains data from selected contact in Apple address book

--- SwiftyContact/SwiftyContact/ViewController.swift
+++ SwiftyContact/SwiftyContact/ViewController.swift
@@ -8,25 +8,52 @@
 
 import UIKit
 import CoreData
+import Contacts
+import ContactsUI
 
-class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate, CNContactPickerDelegate, CNContactViewControllerDelegate {
     
     let appDelegate = UIApplication.sharedApplication().delegate as! AppDelegate
     let managedObjectContext :NSManagedObjectContext! = (UIApplication.sharedApplication().delegate as! AppDelegate).managedObjectContext
     var personsArray = [Persons]()
+    var contactStore = CNContactStore()
     @IBOutlet weak var personsTableView :UITableView!
     
     
     //MARK: Interactivity Methods
     
-    func tableView(tableView: UITableView, commitEditingStyle editingStyle: UITableViewCellEditingStyle, forRowAtIndexPath indexPath: NSIndexPath) {
-        if editingStyle == UITableViewCellEditingStyle.Delete {
-            print("Row to delete:\(indexPath.row)")
-            let personToDelete = personsArray[indexPath.row]
-            managedObjectContext .deleteObject(personToDelete)
-            appDelegate.saveContext()
-            personsArray = fetchPersons()!
-            personsTableView.reloadData()
+    @IBAction func showContactList(sender: UIBarButtonItem) {
+        print("Contacts button pressed")
+        let contactListVC = CNContactPickerViewController()
+        contactListVC.delegate = self
+        presentViewController(contactListVC, animated: true, completion: nil)
+    }
+    
+    func contactPicker(picker: CNContactPickerViewController, didSelectContact contact: CNContact) {
+        print("First Name:\(contact.givenName) Last Name:\(contact.familyName)")
+        
+        for phone in contact.phoneNumbers as [CNLabeledValue] {
+            print("Phone: " + (phone.value as! CNPhoneNumber).stringValue)
+        }
+        
+        for email in contact.emailAddresses as [CNLabeledValue] {
+            print("Email: " + (email.value as! String))
+        }
+        
+        for street in contact.postalAddresses as [CNLabeledValue] {
+            print("Street: " + (street.value as! CNPostalAddress).street)
+        }
+        
+        for city in contact.postalAddresses as [CNLabeledValue] {
+            print("City: " + (city.value as! CNPostalAddress).city)
+        }
+        
+        for state in contact.postalAddresses as [CNLabeledValue] {
+            print("State: " + (state.value as! CNPostalAddress).state)
+        }
+        
+        for zip in contact.postalAddresses as [CNLabeledValue] {
+            print("Zip: " + (zip.value as! CNPostalAddress).postalCode)
         }
     }
     
@@ -44,22 +71,60 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
         return cell
     }
     
+    // swipe to delete
+    func tableView(tableView: UITableView, commitEditingStyle editingStyle: UITableViewCellEditingStyle, forRowAtIndexPath indexPath: NSIndexPath) {
+        if editingStyle == UITableViewCellEditingStyle.Delete {
+            print("Row to delete:\(indexPath.row)")
+            let personToDelete = personsArray[indexPath.row]
+            managedObjectContext .deleteObject(personToDelete)
+            appDelegate.saveContext()
+            personsArray = fetchPersons()!
+            personsTableView.reloadData()
+        }
+    }
+    
+    
+    //MARK: - Contact Methods
+    
+    func requestAccessToContactType(type: CNEntityType) {
+        contactStore.requestAccessForEntityType(type) { (accessGranted: Bool, error: NSError?) -> Void in
+            if accessGranted {
+                print("Granted")
+            } else {
+                print("Not granted")
+            }
+        }
+    }
+    
+    func checkContactAuthorizationStatus(type: CNEntityType) {
+        let status = CNContactStore.authorizationStatusForEntityType(type)
+        switch status {
+        case .NotDetermined:
+            print("Not determined")
+            requestAccessToContactType(type)
+        case .Authorized:
+            print("Authorized")
+        case .Restricted, .Denied:
+            print("Restricted/Denied")
+        }
+    }
+    
     
     //MARK: Core Data Methods
     
-    func tempAddRecords() {
-        let entityDescription :NSEntityDescription = NSEntityDescription.entityForName("Persons", inManagedObjectContext: managedObjectContext)!
-        let currentPerson :Persons! = Persons(entity: entityDescription, insertIntoManagedObjectContext: managedObjectContext)
-        currentPerson.personFirstName = "John"
-        currentPerson.personLastName = "Smith"
-        currentPerson.personStreet = "123 Main Street"
-        currentPerson.personCity = "Washington"
-        currentPerson.personState = "DC"
-        currentPerson.personZip = "22202"
-        currentPerson.personPhone = "843-234-6532"
-        currentPerson.personEmail = "email@gmail.com"
-        appDelegate.saveContext()
-    }
+//    func tempAddRecords() {
+//        let entityDescription :NSEntityDescription = NSEntityDescription.entityForName("Persons", inManagedObjectContext: managedObjectContext)!
+//        let currentPerson :Persons! = Persons(entity: entityDescription, insertIntoManagedObjectContext: managedObjectContext)
+//        currentPerson.personFirstName = "John"
+//        currentPerson.personLastName = "Smith"
+//        currentPerson.personStreet = "123 Main Street"
+//        currentPerson.personCity = "Washington"
+//        currentPerson.personState = "DC"
+//        currentPerson.personZip = "22202"
+//        currentPerson.personPhone = "843-234-6532"
+//        currentPerson.personEmail = "email@gmail.com"
+//        appDelegate.saveContext()
+//    }
     
     func fetchPersons() -> [Persons]? {
         let fetchRequest :NSFetchRequest = NSFetchRequest(entityName: "Persons")
@@ -92,8 +157,9 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
         super.viewDidLoad()
 //        tempAddRecords()
         personsArray = fetchPersons()!
-        let firstPerson = personsArray.first
-        print("Number of Persons:\(personsArray.count) First Record:\(firstPerson?.personFirstName)")
+//        let firstPerson = personsArray.first
+//        print("Number of Persons:\(personsArray.count) First Record:\(firstPerson?.personFirstName)")
+        checkContactAuthorizationStatus(CNEntityType.Contacts)
 
     }
     
